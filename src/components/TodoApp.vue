256f3b576b1f19b0e9873002802217c5dccc0e80
jeongnaehyeok <cnh0301@gmail.com>
2020-06-19 14:32:47 +0900

    todo 입력

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="todo-app">
-        test
+        <todo-form @onSubmit="onSubmit"/>
     </div>
 </template>
 
 <script>
+import TodoForm from './TodoForm'
+
 export default {
     name: 'TodoApp',
+    components:{
+        TodoForm
+    },
+    methods:{
+        onSubmit(payload){
+            this.todos.push(payload)
+            this.storeTodo()
+        },
+        storeTodo(){
+            const todoToString = JSON.stringify(this.todos)
+            localStorage.setItem('todos', todoToString)
+        }
+    },
+    data(){
+        return{
+            todos: [],
+        }
+    },
+    created(){
+        this.todos = localStorage.todos ? JSON.parse(localStorage.todos) : [];
+    }
 }
 </script>
 
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="onSubmit">
+        <input type="text"
+            v-model="todo"
+            placeholder="오늘은 무엇을 해야할까?">
+        <button type="submit">등록</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'TodoForm',
+    methods:{
+        onSubmit(){
+            const { todo } = this
+            if(!todo)return false
+            const id = new Date().getTime()
+            this.$emit('onSubmit', { todo, id })
+            this.todo = ''
+        }
+    },
+    data(){
+        return{
+            todo:'',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
